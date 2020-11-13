116071f8447d7b298966b050150ea21a7d7175c5
David Bray <david@brayworth.com.au>
2020-11-13 12:41:13 +1000

    getting going ..

---
+++ dao/db/property_photolog.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace dao;
+
+$dbc = \sys::dbCheck( 'property_photolog' );
+
+$dbc->defineField('created', 'datetime');
+$dbc->defineField('updated', 'datetime');
+$dbc->defineField('dirModTime', 'datetime');
+$dbc->defineField('dirStats', 'varbinary', 128);
+$dbc->defineField('property_id', 'bigint');
+$dbc->defineField('subject', 'varchar', 100 );
+$dbc->defineField('public_link', 'varchar');
+$dbc->defineField('public_link_expires', 'date');
+$dbc->defineField('date', 'date');
+$dbc->defineField('notes', 'text');
+$dbc->check();
