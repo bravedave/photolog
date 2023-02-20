7bca4fad68f90d88040fb387764c7fa029aca808
Dave Bray <david@brayworth.com.au>
2023-02-20 12:01:25 +1000

    quieten down

--- src/photolog/postUpdate.php
+++ src/photolog/postUpdate.php
@@ -11,20 +11,21 @@
 namespace photolog;
 
 use application;
+use bravedave\dvc\logger;
 use dvc;
 use green;
 use smokealarm;
 
 class postUpdate extends dvc\service {
   protected function _upgrade() {
-    config::route_register( 'photolog', 'photolog\\controller');
-    config::route_register( 'people', 'green\\people\\controller');
+    config::route_register('photolog', 'photolog\\controller');
+    config::route_register('people', 'green\\people\\controller');
     // config::route_register( 'property', 'green\\properties\\controller'); // for some cms compatibility
-    config::route_register( 'properties', 'green\\properties\\controller');
-    config::route_register( 'beds', 'green\\beds_list\\controller');
-    config::route_register( 'baths', 'green\\baths\\controller');
-    config::route_register( 'property_type', 'green\\property_type\\controller');
-    config::route_register( 'postcodes', 'green\\postcodes\\controller');
+    config::route_register('properties', 'green\\properties\\controller');
+    config::route_register('beds', 'green\\beds_list\\controller');
+    config::route_register('baths', 'green\\baths\\controller');
+    config::route_register('property_type', 'green\\property_type\\controller');
+    config::route_register('postcodes', 'green\\postcodes\\controller');
 
     green\beds_list\config::green_beds_list_checkdatabase();
     green\baths\config::green_baths_checkdatabase();
@@ -35,35 +36,30 @@ class postUpdate extends dvc\service {
 
     green\people\config::green_people_checkdatabase();
     green\properties\config::green_properties_checkdatabase();
-    echo( sprintf('%s : %s%s', 'green updated', __METHOD__, PHP_EOL));
+    echo (sprintf('%s : %s%s', 'green updated', __METHOD__, PHP_EOL));
 
     smokealarm\config::smokealarm_checkdatabase();
-    config::route_register( 'smokealarm', 'smokealarm\controller');
-    config::route_register( 'smokealarmtypes', '');
-    config::route_register( 'smokealarmlocations', 'smokealarm\controllerSmokeAlarmLocations');
-    echo( sprintf('%s : %s%s', 'smokealarm  updated', __METHOD__, PHP_EOL));
+    config::route_register('smokealarm', 'smokealarm\controller');
+    config::route_register('smokealarmtypes', '');
+    config::route_register('smokealarmlocations', 'smokealarm\controllerSmokeAlarmLocations');
+    echo (sprintf('%s : %s%s', 'smokealarm  updated', __METHOD__, PHP_EOL));
 
     config::photolog_checkdatabase();
-    echo( sprintf('%s : %s%s', 'photolog  updated', __METHOD__, PHP_EOL));
-
+    echo (sprintf('%s : %s%s', 'photolog  updated', __METHOD__, PHP_EOL));
   }
 
   static function upgrade() {
-    $app = new self( application::startDir());
+    $app = new self(application::startDir());
     $app->_upgrade();
-
   }
 
   protected function _cron() {
     utility::stamp();
-    \sys::logger( sprintf('<%s> %s', 'complete', __METHOD__));
-
+    logger::info(sprintf('<%s> %s', 'complete', __METHOD__));
   }
 
   static function cron() {
-    $app = new self( application::startDir());
+    $app = new self(application::startDir());
     $app->_cron();
-
   }
-
 }
